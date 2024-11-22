0fee27bcbd768083e32a0fb95ecdd7271b96b36e
editorbank <>
2024-11-22 23:46:24 +0300

    windows - complete

--- tests/meltano/test.cmd
+++ tests/meltano/test.cmd
@@ -1,7 +1,7 @@
 call ..\tests-common.cmd && echo on || exit /b 1
 
-@REM meltano lock --update --all || exit /b 1
+meltano lock --update --all || exit /b 1
 meltano install || exit /b 1
-@REM meltano test || exit /b 1
-@REM for /F "usebackq" %%I in ( ` type meltano.yml ^| yq ".plugins[][].name" ` ) do meltano invoke %%I --help >nul || exit /b 1
+meltano test || exit /b 1
+for /F "usebackq" %%I in ( ` type meltano.yml ^| yq ".plugins[][].name" ` ) do meltano invoke %%I --help >nul || exit /b 1
 echo OK
