8dd06aae7604a5540d7f95c40f3fab7fed60d1f6
editornank <>
2025-08-14 16:54:48 +0300

    Чт 14 авг 2025 16:54:48 MSK

--- tests/meltano/test.cmd
+++
@@ -1,7 +0,0 @@
-call ..\tests-common.cmd && echo on || exit /b 1
-
-meltano lock --update --all || exit /b 1
-meltano install -p 1 || exit /b 1
-meltano test || exit /b 1
-for /F "usebackq" %%I in ( ` type meltano.yml ^| yq ".plugins[][].name" ` ) do meltano invoke %%I --help >nul || exit /b 1
-echo OK
--- tests/meltano/test.sh
+++
@@ -1,8 +0,0 @@
-set -e
-. ../../ve.sh
-
-meltano lock --update --all
-meltano install
-meltano test
-cat meltano.yml | yq '.plugins[][].name' | xargs -i -tn1 meltano invoke {} --help >/dev/null
-echo OK
