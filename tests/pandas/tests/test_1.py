be8c6e97d72b307b848129a971ef915f82c2d3a3
editornank <>
2025-08-14 16:22:22 +0300

    Чт 14 авг 2025 16:22:22 MSK

--- tests/pandas/tests/test_1.py
+++ tests/pandas/tests/test_1.py
@@ -9,5 +9,6 @@ def test_1():
   df = pd.read_csv("data/test20.csv")
   df.to_excel(".tmp/test20.xlsx", index=False)
   df = pd.read_excel(".tmp/test20.xlsx") 
-  df.to_csv(".tmp/test20.csv", index=False, line_terminator='\n')
+  # df.to_csv(".tmp/test20.csv", index=False, line_terminator='\n') # ранних версий
+  df.to_csv(".tmp/test20.csv", index=False, lineterminator='\n')
   assert filecmp.cmp("data/test20.csv",".tmp/test20.csv")
